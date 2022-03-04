eee4ccd2998d84067b2fb4f49aa30b1d5dd9cef5
Pradeepak5 <pradeepak571@gmail.com>
2022-03-04 16:13:25 +0530

    Python Collections and Tasks

---
+++ list1_10.py
@@ -0,0 +1,12 @@
+list=[]
+for i in range(2,101):
+    list.append(i)
+
+print(list)
+divisible_7=[]
+
+for i in list:
+    if i%7==0:
+       divisible_7.append(i)
+
+print(divisible_7)
---
+++ list_comprehension.py
@@ -0,0 +1,6 @@
+list=[i for i in range(1,101) if i%7==0 ]
+print(list)
+
+
+list1=[i.upper() for i in ["pradeep","deepak","naveen"]]
+print(list1)
